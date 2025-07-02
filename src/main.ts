8996676b285dd1f3c582642c784df082c9e48bb6
retosugua <renanaugusto.ras@gmail.com>
2025-07-02 17:31:39 -0300

    add loading

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,3 @@
 import './styles/main.scss'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+
