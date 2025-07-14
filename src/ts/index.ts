09497fec222cd285504d09009c8fd4b5ddc1adf5
retosugua <renanaugusto.ras@gmail.com>
2025-07-14 16:38:29 -0300

    arrumando animacao

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,10 +1,14 @@
+const progress = document.querySelector('.progress-bar') as HTMLButtonElement;
+const img = document.querySelector('.img-animada') as HTMLImageElement;
 const statusMsg = document.getElementById('status') as HTMLButtonElement;
-const errorMsg = document.getElementById('error')  as HTMLButtonElement;
+// const errorMsg = document.getElementById('error') as HTMLButtonElement;
 
 const steps = [
     "Carregando interface...",
     "Conectando ao servidor...",
     "Verificando dados...",
+    "Autenticando usuário...",
+    "Retornando dados...",
     "Finalizando processo..."
 ];
 
@@ -22,14 +26,14 @@ const updateMessage = (msg: any) => {
 const stepInterval = setInterval(() => {
     updateMessage(steps[index]);
     index = (index + 1) % steps.length;
-}, 2000);
+}, 3000);
 
 
 setTimeout(() => {
     clearInterval(stepInterval);
     updateMessage("Enviando requisição...");
 
-    fetch("https://jsonplaceholder.typicode.com/posts/1") 
+    fetch("https://jsonplaceholder.typicode.com/posts/100000")
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Erro ${response.status}: ${response.statusText}`);
@@ -39,12 +43,21 @@ setTimeout(() => {
         .then(() => {
             updateMessage("Sucesso! Redirecionando...");
             setTimeout(() => {
-                window.location.href = "https://euro17.com.br"; 
+                // window.location.href = "https://euro17.com.br"; 
+                window.location.replace("https://euro17.com.br");
             }, 1500);
         })
         .catch(error => {
             updateMessage("Algo deu errado!");
-            errorMsg.textContent = error.message;
+            console.error(error);
+            // errorMsg.textContent = error.message;
         });
 
-}, 9000); 
+}, 9000);
+
+
+img.addEventListener('animationend', () => {
+    img.classList.add('final');
+
+    progress.classList.add('visible');    
+});
